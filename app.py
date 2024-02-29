b60a3c91c7b64219d37983766ad977db8471ed4a
bybuss <145790087+bybuss@users.noreply.github.com>
2024-02-29 19:09:15 +0000

    Pop`ы

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("index.html", title="Hello")
+    if request.method == "POST":
+        file_name = request.form.get("file_name")
+        context = request.form.get("context")
+
+        with open(f"files/{file_name}.txt", "w") as f:
+            f.write(context)
+
+        return render_template("created.html", file_name=file_name)
+        
+    return render_template("main.html", title="DOP`ы")
+
+
+@app.route("/all")
+def all_files():
+    files_lst = os.listdir("./files")
+
+    return render_template('all.html', files=files_lst)
+
+
+@app.route("/file/<path:file_name>")
+def file_card(file_name):
+    # Добавьте расширение .txt к file_name, если оно не включено в URL
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+    path = f"./files/{file_name}"
+    try:
+        with open(path, "r") as f:
+            content = f.read()
+    except FileNotFoundError:
+        content = "Файл не найден"
+    return render_template('file_card.html', content=content, file_name=file_name)
+
+
